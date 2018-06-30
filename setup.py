c725ee3c0281303f1ab138100fe35fb62b7e0af4
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2018-06-30 20:24:32 +0400

    pypi settings

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+
+with open("README.md", "r") as fhandle:
+    long_description = fhandle.read()
+
+setuptools.setup(
     name = 'tester_gae',
-    packages = ['tester_gae'],
-    version = 'v1.0',
-    description = 'Class for autotests GoogleAppEngine platform python apps',
+    version = '1.0',
     author = 'Vitaly Bogomolov',
     author_email = 'mail@vitaly-bogomolov.ru',
+    description = 'Class for autotests GoogleAppEngine platform python apps',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url = 'https://github.com/vb64/test.helper.gae',
+    packages = ['tester_gae'],
     download_url = 'https://github.com/vb64/test.helper.gae/archive/v1.0.tar.gz',
     keywords = ['python', 'Python27', 'GAE', 'GoogleAppEngine', 'unittest'],
-    classifiers = [],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
